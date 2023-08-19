71e1214baa64a732d304bb7bb85ed7fc778d901d
nilesh <nilesh.p220@gmail.com>
2023-08-19 17:28:25 +0000

    real code

---
+++ code.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region     = "ap-south-1"
+  
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  count         = 1
+
+  tags = {
+    Name = "my-ec2"
+  }
+}
+
+resource "aws_instance" "db" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  count         = 1
+
+  tags = {
+    Name = "my-db"
+  }
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "nileshterraform11"
+    key    = "project"
+    region = "ap-south-1"
+    access_key = "AKIAW6DHWWT6VAK4Y64V"
+    secret_key = "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"
+    dynamodb_table ="nileshDB"
+
+}
+}
+
